d34242328f6d0c03772a56424fb6f16f80305830
Roman Kostyuchenko <76057139+GitRockie@users.noreply.github.com>
2021-06-14 11:48:19 +0200

    fins el Nivell 2

---
+++ dist/js/index.js
@@ -0,0 +1,91 @@
+/**
+ * --------------------------------------------------------------------------
+ * Bootstrap (v4.6.0): index.js
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
+ * --------------------------------------------------------------------------
+ */
+
+import Alert from './src/alert'
+import Button from './src/button'
+import Carousel from './src/carousel'
+import Collapse from './src/collapse'
+import Dropdown from './src/dropdown'
+import Modal from './src/modal'
+import Popover from './src/popover'
+import Scrollspy from './src/scrollspy'
+import Tab from './src/tab'
+import Toast from './src/toast'
+import Tooltip from './src/tooltip'
+import Util from './src/util'
+
+export {
+  Util,
+  Alert,
+  Button,
+  Carousel,
+  Collapse,
+  Dropdown,
+  Modal,
+  Popover,
+  Scrollspy,
+  Tab,
+  Toast,
+  Tooltip
+}
+
+var TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 300 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+  document.body.appendChild(css);
+};
---
+++ dist/js/validate.js
@@ -0,0 +1,109 @@
+const form = document.getElementById('myFormId');
+
+function registerValidate() {
+	var acumErrores = 0;
+	
+	form.classList.remove('is-invalid');
+	
+	//var inputEmail = document.forms["myForm"]["inputEmail"];
+
+
+	var inputFirstName = document.getElementById('inputFirstName');
+	var inputLastName = document.getElementById('inputLastName');
+	var inputEmail = document.getElementById('inputEmail');
+	var inputPassword = document.forms["myForm"]["inputPassword"];
+	var inputSoSecNum = document.forms["myForm"]["inputSoSecNum"];
+	var inputCountry = document.forms["myForm"]["inputCountry"];
+	var inputCity = document.forms["myForm"]["inputCity"];
+	var inputZipCode = document.forms["myForm"]["inputZipCode"];
+	var gridCheck = document.forms["myForm"]["gridCheck"];
+	
+	// FistName Validation
+	if(inputFirstName.value == "") {
+		inputFirstName.classList.add("is-invalid");
+		document.getElementById("errorFirstName").textContent = "Complete this camp, please";
+        acumErrores ++;
+    }
+	// LastName Validation
+	if(inputLastName.value == "") {
+		inputLastName.classList.add("is-invalid");
+		document.getElementById("errorLastName").textContent = "Complete this camp, please";
+        acumErrores ++;
+    }
+	//E-mail Validation amb RegEx
+	if(inputEmail.value == "") {
+		inputEmail.classList.add("is-invalid");
+		document.getElementById("errorEmail").textContent = "Complete this camp, please";
+        acumErrores ++;
+    }else if(!validar_email(inputEmail.value)){
+		inputEmail.classList.add("is-invalid");
+		document.getElementById("errorEmail").textContent = "This e-mail does not comply with the format";
+		acumErrores ++;
+	}
+	// Validació de la contrasenya amb RegEx
+    if(inputPassword.value == "") {
+		inputPassword.classList.add("is-invalid");
+		document.getElementById("errorPassword").textContent = "Complete this camp of password, please";
+		acumErrores ++;
+	}else if(!validar_password(inputPassword.value)){
+		inputPassword.classList.add("is-invalid");
+		document.getElementById("errorPassword").textContent = "This password does not comply with the format";
+		acumErrores ++;
+	}
+	//Validació del Número de SS 
+    if(inputSoSecNum.value == "") {
+		inputSoSecNum.classList.add("is-invalid");
+		document.getElementById("errorSoSecNum").textContent = "Complete this camp of social security number, please";
+		acumErrores ++;
+	}
+	//Pais
+    if(inputCountry.value == "") {
+		inputCountry.classList.add("is-invalid");
+		document.getElementById("errorCountry").textContent = "Complete this camp of country, please";
+		acumErrores ++;
+	}
+	//Ciutat Validació
+	if(inputCity.value == "") {
+		inputCity.classList.add("is-invalid");
+		document.getElementById("errorCity").textContent = "Complete this camp of city, please";
+		acumErrores ++;
+	}
+	//CP
+	if(inputZipCode.value == "" || inputZipCode.length<5) {
+		inputZipCode.classList.add("is-invalid");
+		document.getElementById("errorZipCode").textContent = "This zip code does not comply with the format";
+		acumErrores ++;
+	}
+	//Acceptació 
+	if(!gridCheck.checked) {
+		gridCheck.classList.add("is-invalid");
+		document.getElementById("errorCheck").textContent = "Accept this Terms and Conditions, please";
+		acumErrores ++;
+	}
+	
+    if (acumErrores > 0){
+        return false;
+    }else{
+		return true;
+	}
+}
+
+
+
+form.addEventListener('blur', (event) => {
+	console.log(event);
+	if(event.target.value!='') event.target.classList.remove('is-invalid');
+    //registerValidate();
+}, true);
+
+//Validació e-mail
+function validar_email(email) {
+	var regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	return regex.test(email) ? true : false;
+}
+
+//Creo la funció per validar la contrasenya Nivell 2
+function validar_password(password) {
+	var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+	return regex.test(password) ? true : false;
+}
